5a4440502a1200a73cd1dd7e1b35f4448fd58d17
luisSantiagoLefewaresolutions <139481193+luisSantiagoLefewaresolutions@users.noreply.github.com>
2023-08-01 18:40:06 -0500

    Update SynADOtoGitHub.ps1

--- SynADOtoGitHub.ps1
+++ SynADOtoGitHub.ps1
@@ -19,9 +19,9 @@ param(
 Write-Host ' - - - - - - - - - - - - - - - - - - - - - - - - -'
 Write-Host ' reflect Azure Devops repo changes to GitHub repo'
 Write-Host ' - - - - - - - - - - - - - - - - - - - - - - - - - '
-# $AzureRepoName = "Azure-MasterIaCTemplates"
-# $ADOCloneURL = "dev.azure.com/LefeWare-Solutions/LefeWareSolutions/_git/Azure-MasterIaCTemplates"
-# $GitHubCloneURL = "github.com/LefeWare-Solutions/Azure-MasterIaCTemplates.git"
+ $AzureRepoName = "Azure-MasterIaCTemplates"
+ $ADOCloneURL = "dev.azure.com/LefeWare-Solutions/LefeWareSolutions/_git/Azure-MasterIaCTemplates"
+ $GitHubCloneURL = "github.com/LefeWare-Solutions/Azure-MasterIaCTemplates.git"
 $stageDir = pwd | Split-Path
 Write-Host "stage Dir is : $stageDir"
 $githubDir = $stageDir +"\"+"gitHub"
